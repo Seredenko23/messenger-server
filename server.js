93f21b8fe084fe1bcbf25086339813ce8f8b6744
Vlad <srvladyk555@gmail.com>
2020-02-05 14:06:05 +0200

    added validation, login, register, token

--- server.js
+++ server.js
@@ -1,125 +1,152 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const MONGO_URL = process.env.MONGO_URL;
-const port = process.env.PORT;
-const client = new MongoClient(MONGO_URL, { useUnifiedTopology: true });
-const ObjectID = require('mongodb').ObjectID;
-const jwt = require('jsonwebtoken');
+const express = require('express')
+const bodyParser = require('body-parser')
+const ObjectID = require('mongodb').ObjectID
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+const User = require('./models/User')
+const bcrypt = require('bcryptjs')
+const { registerValidation, loginValidation } = require('./controller/validation')
 
-const app = express();
-let db;
-let refreshTokens = [];
+const MONGO_URL = process.env.MONGO_URL
+const port = process.env.PORT
+const app = express()
+let db
+const refreshTokens = []
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/users',(req, res) =>{
-    db.collection('users').find().toArray((err,docs) => {
-        if (err) {
-            console.log(err);
-         return res.sendStatus(500);
-        }
-        res.send(docs)
-    })
-});
+app.get('/users', authentificateToken,(req, res) => {
+  // db.collection('users').find().toArray((err, docs) => {
+  //   if (err) {
+  //     console.log(err)
+  //     return res.sendStatus(500)
+  //   }
+  //   res.send(docs)
+  // })
+  res.send('great')
+})
 
 app.get('/users/:id', (req, res) => {
-    db.collection('users').findOne({ _id : ObjectID(req.params.id ) }, (err, doc) => {
-        if (err) {
-            console.log(err);
-            return res.sendStatus(500);
-        }
-        res.send(doc)
+  db.collection('users').findOne({ _id: ObjectID(req.params.id) }, (err, doc) => {
+    if (err) {
+      console.log(err)
+      return res.sendStatus(500)
+    }
+    res.send(doc)
+  })
+})
+
+app.post('/users', async (req, res) => {
+
+  const { error } = registerValidation(req.body)
+  if(error) return res.status(400).send(error.details[0].message)
+
+  const emailExist = await User.findOne({email: req.body.email})
+  if (emailExist) return res.status(400).send('Email already exist')
+
+  const salt = await bcrypt.genSalt(10)
+  const hashPassword = await bcrypt.hash(req.body.password, salt)
+
+  const user = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    username: req.body.username,
+    email: req.body.email,
+    password: hashPassword
+  })
+
+  try {
+    const savedUser = await user.save()
+    res.send(savedUser)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
+app.put('/users/:id', (req, res) => {
+  db.collection('users').update(
+    { _id: ObjectID(req.params.id) },
+    { email: req.body.email },
+    function (err, result) {
+      if (err) {
+        console.log(err)
+        return res.sendStatus(500)
+      }
+      res.sendStatus(200)
+    }
+  )
+})
+
+app.delete('/users/:id', (req, res) => {
+  db.collection('users').deleteOne(
+    { _id: ObjectID(req.params.id) },
+    function (err, result) {
+      if (err) {
+        console.log(err)
+        return res.sendStatus(500)
+      }
+      res.sendStatus(200)
     })
-});
-
-app.post('/users', (req, res) => {
-    let user = {
-        email: req.body.email
-    };
-    db.collection('users').insert(user, (err, result) => {
-        if (err){
-            console.log(err);
-         return res.sendStatus(500);
-        }
-        res.send(user);
-    })
-});
-
-app.put('/users/:id', (req,res) => {
-    db.collection('users').update(
-        { _id: ObjectID(req.params.id) },
-        { email: req.body.email },
-        function (err, result) {
-            if (err) {
-                console.log(err);
-                return res.sendStatus(500);
-            }
-            res.sendStatus(200);
-        }
-    )
-});
-
-app.delete('/users/:id', (req,res) => {
-    db.collection('users').deleteOne(
-        { _id: ObjectID(req.params.id) },
-        function (err, result) {
-            if (err) {
-                console.log(err);
-                return res.sendStatus(500);
-            }
-            res.sendStatus(200);
-        })
-});
+})
 
 // Refresh by token
 app.post('/token', (req, res) => {
-    const refreshToken = req.body.token;
-    if (refreshToken == null) return res.sendStatus(401);
-    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-        console.log(user);
-        if (err) return res.sendStatus(403);
-        const accessToken = generateAccessToken({ name: user.username })
-        res.json({ accessToken: accessToken })
-    })
-});
+  const refreshToken = req.body.token
+  if (refreshToken == null) return res.sendStatus(401)
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    console.log(user)
+    if (err) return res.sendStatus(403)
+    const accessToken = generateAccessToken({ name: user.username })
+    res.json({ accessToken: accessToken })
+  })
+})
 
-app.post('/login', (req, res) => {
-    const username = req.body.name;
-    const user = { email: username };
+app.post('/login', async (req, res) => {
+  const { error } = loginValidation(req.body)
+  if(error) return res.status(400).send(error.details[0].message)
 
-    const accessToken = generateAccessToken(user);
-    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
-    refreshTokens.push(refreshToken);
-    res.json({ accessToken: accessToken, refreshToken: refreshToken })
-});
+  const user = await User.findOne({email: req.body.email})
+  if (!user) return res.status(400).send('Invalid email')
+
+  const validPass = await bcrypt.compare(req.body.password, user.password)
+  if(!validPass) return res.status(400).send('Invalid password')
+
+  let token = generateAccessToken({_id: user._id})
+  res.header('Authorization', `Bearer ${token}`);
+  res.send(user)
+  // const accessToken = generateAccessToken(user)
+  // const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+  // refreshTokens.push(refreshToken)
+  // res.json({ accessToken: accessToken, refreshToken: refreshToken })
+})
 
 function authentificateToken (req, res, next) {
-    const authHeader = req.headers.authorization;
-    const token = authHeader && authHeader.split(' ')[1];
-    if (token == null) return res.sendStatus(401);
+  const authHeader = req.headers.authorization
+  const token = authHeader && authHeader.split(' ')[1]
+  if (token == null) return res.sendStatus(401)
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user;
-        next()
-    })
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+    req.user = user
+    next()
+  })
 }
 
 function generateAccessToken (user) {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' })
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30s' })
 }
 
-client.connect((err,database) => {
+mongoose.connect(MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, database) => {
     if (err) {
-        return console.log(err);
+      return console.log(err)
     }
-    db = database.db('messenger');
     app.listen(port, () => {
-        console.log(`Server is listening on port: ${9000}`);
+      console.log(`Server is listening on port: ${9000}`)
     })
-});
+  })
